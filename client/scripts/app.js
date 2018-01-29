4a54609adb2741f93791bc6f2663345739867fc8
Becca Stanger <beccastanger@gmail.com>
2018-01-29 12:43:07 -0800

    Pass all tests - but need fix comparator event to sort

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,7 +5,8 @@ var Movie = Backbone.Model.extend({
   },
 
   toggleLike: function() {
-    // your code here
+    var newLikeBool = !this.get('like');
+    this.set('like', newLikeBool);
   }
 
 });
@@ -15,13 +16,15 @@ var Movies = Backbone.Collection.extend({
   model: Movie,
 
   initialize: function() {
-    // your code here
+    // this.on('change', this.sort, this);
+    this.on('change:like', this.sort, this)
   },
 
   comparator: 'title',
 
   sortByField: function(field) {
-    // your code here
+    this.comparator = field;
+    this.sort(); // cheating...
   }
 
 });
@@ -58,7 +61,7 @@ var MovieView = Backbone.View.extend({
                         </div>'),
 
   initialize: function() {
-    // your code here
+    this.model.on('change', this.render, this);
   },
 
   events: {
@@ -66,7 +69,7 @@ var MovieView = Backbone.View.extend({
   },
 
   handleClick: function() {
-    // your code here
+    this.model.toggleLike();
   },
 
   render: function() {
@@ -79,7 +82,7 @@ var MovieView = Backbone.View.extend({
 var MoviesView = Backbone.View.extend({
 
   initialize: function() {
-    // your code here
+    this.collection.on('sort', this.render, this);
   },
 
   render: function() {
